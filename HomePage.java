4e27fd62080ddd9d8fefb459ecb9257618bbe890
zableytan <148207827+zableytan@users.noreply.github.com>
2023-10-21 04:05:01 +0800

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,43 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import androidx.cardview.widget.CardView;
+
+public class HomePage extends AppCompatActivity {
+
+    private CardView exitCardView;
+    private CardView aboutUsCardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        exitCardView = findViewById(R.id.exitCardView);
+        aboutUsCardView = findViewById(R.id.aboutUsCardView);
+
+        exitCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform logout action and go back to the login page (MainActivity)
+                Intent intent = new Intent(HomePage.this, MainActivity.class);
+                // Add a flag to clear the back stack so the user can't go back to the home page
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        aboutUsCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String facebookUrl = "https://www.facebook.com/basleh.tan";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private TextView signupText;
+
+    // Hardcoded username and password
+    private static final String CORRECT_USERNAME = "bazleytan";
+    private static final String CORRECT_PASSWORD = "nayskamaamkeana";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        signupText = findViewById(R.id.signupText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredUsername = usernameEditText.getText().toString();
+                String enteredPassword = passwordEditText.getText().toString();
+
+                if (enteredUsername.equals(CORRECT_USERNAME) && enteredPassword.equals(CORRECT_PASSWORD)) {
+                    Intent intent = new Intent(MainActivity.this, HomePage.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                    usernameEditText.setText(""); // Clear username text
+                    passwordEditText.setText(""); // Clear password text
+                }
+            }
+        });
+
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
